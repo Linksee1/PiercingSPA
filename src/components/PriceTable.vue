ede00dde8cd245fc8a9c925b233d270fac1747f5
linksee1 <linksee1@yandex.ru>
2024-11-12 12:51:44 +0300

    Some table markup

--- src/components/PriceTable.vue
+++ src/components/PriceTable.vue
@@ -3,7 +3,65 @@
 </script>
 
 <template>
+  <div class="price-table-wrapper">
+    <ul class="price-table">
+      <li class="price-table-item">
+        <span>Пирсинг</span>
+        <span>от 1000 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Прокол ушей</span>
+        <span>от 2000 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Хеликс-пирсинг</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Септум-пирсинг</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Индастриал-пирсинг</span>
+        <span>от 1400 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Прокол пупка</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Прокол языка</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Прокол брови</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Даунсайз
+          <span class="hover-popup">*<span class="hover-popup-text">Подсказка</span></span>
+        </span>
+        <span>от 200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Замена украшения</span>
+        <span>от 100 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Пирсинг интимной зоны</span>
+        <span>от 2000 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Трагус-пирсинг</span>
+        <span>от 1200 ₽</span>
+      </li>
+      <li class="price-table-item">
+        <span>Украшения</span>
+        <span>от 200 ₽ до 1800 ₽</span>
+      </li>
+    </ul>
 
+  </div>
 </template>
 
 <style scoped>
